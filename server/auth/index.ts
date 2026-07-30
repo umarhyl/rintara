00f0d0a0c23c6a5bad6d126aca4864b2884a0dad
umarhyl <umarbaharun941@gmail.com>
2026-07-31 00:04:33 +0700

    refactor: remove unused components

--- server/auth/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./authorization";
-export * from "./adapters";
-export * from "./identity";
-export * from "./onboarding";
-export * from "./policies";
-export * from "./schemas";
-export * from "./types";
